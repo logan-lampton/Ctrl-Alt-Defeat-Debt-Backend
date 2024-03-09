0e9e97372555a503484eb4c513297d910af527db
Jesse Ilc <jesseilc123@gmail.com>
2024-03-09 11:45:50 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from config import app
+
+# docker build -t python-server .
+# docker run -p 5555:5555 python-server
+if __name__ == '__main__':
+    app.run(port=5555, host="0.0.0.0",  debug=True)
