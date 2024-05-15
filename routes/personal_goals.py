3bd6e6f798aba33b58b47341729e36bc092e5ed3
njerimk <nmurugikamau@gmail.com>
2024-05-15 15:54:53 -0400

    added patch request to goals

--- routes/personal_goals.py
+++ routes/personal_goals.py
@@ -15,6 +15,7 @@ class Personal_goals(Resource):
 
         return list, 200
     
+
     def post(self):
         user_id = session.get("user_id")
         if not user_id:
@@ -36,9 +37,10 @@ class Personal_goals(Resource):
         
         db.session.add(new_personal_goal)
         db.session.commit()
+        
 
         return new_personal_goal.to_dict(), 201
-    
+
     def delete(self):
         id = request.get_json()["id"]
         personal_goal = Personal_goal.query.filter(Personal_goal.id == id).first()
@@ -48,18 +50,29 @@ class Personal_goals(Resource):
 
         return {"message": "Personal goal successfully deleted"}, 200
     
-    # def patch(self):
-    #     user_id = session.get("user_id")
-    #     if not user_id:
-    #         return {"message": "Unauthorized"}, 401
-        
-    #     id = request.get_json()["id"]
-    #     personal_goal = Personal_goal.query.filter(Personal_goal.id == id).first()
-    #     setattr(personal_goal, "amount_saved", request.get_json()["amount_saved"])
-        
-    #     db.session.add(personal_goal)
-    #     db.session.commit()
+    def patch(self):
+        user_id = session.get("user_id")
+        if not user_id:
+            return {"message": "Unauthorized"}, 401
+        new_goal = Personal_goal(
+           name=request.get_json()["name"],
+           saving_target=request.get_json()["saving_target"],
+           user_id=request.get_json()["user_id"],
+           emoji=request.get_json()["emoji"],
+        )
 
-    #     return personal_goal.to_dict(), 201
+        #logic for converting string datetime in python object
+        end_timeframe_str = request.get_json()["end_timeframe"]
+        year = int(end_timeframe_str[0:4])
+        month = int(end_timeframe_str[5:7])
+        day = int(end_timeframe_str[8:10])
+        end_timeframe = datetime(year, month, day)
+
+        new_goal.end_timeframe = end_timeframe
+
+        db.session.add(new_goal)
+        db.session.commit()
+
+        return new_goal.to_dict(), 201
     
 api.add_resource(Personal_goals, "/personal_goals")
